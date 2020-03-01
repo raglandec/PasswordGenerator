3bec43e54f1937be16f73395212a7822ee2c7b0a
Eric Ragland <raglandec@gmail.com>
2020-02-29 20:58:52 -0500

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PasswordGenerator
 This will be a password generator application.
+
+In this application, I made a password generator that can generate a password between 8 to 128 characters and whether or not to include uppercase or lowercase letters, special characters, and numbers. Once the password is generated, the user can then decide to click on the clipboard icon to copy the password for other uses.
+
+The container for the generator can adapt to different heights and widths for different devices. 
+
+I struggled with aligning the clipboard image into a button but couldn't figure out how to directly correlate the html with js. Also, I couldn't figure out how to make the max out the length of the passoword to 128 characters. There is no specific max.
+
+I have provided different executions within the javascript.
--- javascript.js
+++ javascript.js
@@ -1,84 +1,88 @@
 window.onload= function (){
+	// window onload loads the html and css elements before loading the javascript to locate the elements
+	const resultEl = document.getElementById('result');
+	const lengthEl = document.getElementById('length');
+	const uppercaseEl = document.getElementById('uppercase');
+	const lowercaseEl = document.getElementById('lowercase');
+	const numbersEl = document.getElementById('numbers');
+	const symbolsEl = document.getElementById('symbols');
+	const generateEl = document.getElementById('generate');
+	const clipboard = document.getElementById('clipboard');
+// created variables to locate different element ids of the page
 
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
+	const randomFunc = {
+		lower: getRandomLower,
+		upper: getRandomUpper,
+		number: getRandomNumber,
+		symbol: getRandomSymbol
+	}
+// loads the settings uppercase, lowercase, numerical, and special character settings funtion into a function for creating the password
 
-const randomFunc = {
-    upper: getRandomUpper,
-	lower: getRandomLower,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
-}
+// click operator to copy the password	
+	clipboard.addEventListener('click', () => {
+		const textarea = document.createElement('textarea');
+		const password = resultEl.innerText;
+		
+		if(!password) { return; }
+		
+		textarea.value = password;
+		document.body.appendChild(textarea);
+		textarea.select();
+		document.execCommand('copy');
+		textarea.remove();
+		alert('Copied!');
+	});
 
-clipboardEl.addEventListener('click', () => {
-	const textarea = document.createElement('textarea');
-	const password = resultEl.innerText;
-	
-	if(!password) { return; }
-	
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand('copy');
-	textarea.remove();
-	alert('Password copied to clipboard');
-});
+// click operator to generate a password
+	generate.addEventListener('click', () => {
+		const length = +lengthEl.value;
+		const hasLower = lowercaseEl.checked;
+		const hasUpper = uppercaseEl.checked;
+		const hasNumber = numbersEl.checked;
+		const hasSymbol = symbolsEl.checked;
+	// if the settings are checkes, then the correlating function will run
+		resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+	});
+	// it does what it says, generates the password provided the different settings 
+	function generatePassword(lower, upper, number, symbol, length) {
+		let generatedPassword = '';
+		const typesCount = lower + upper + number + symbol;
+		const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+		
+		// function if the are no selected settings; there will be no value return
+		if(typesCount === 0) {
+			return '';
+		}
+		
+	// created a loop for the length of the password to choose specific elements at random for the provided length
+		for(let i=0; i<length; i+=typesCount) {
+			typesArr.forEach(type => {
+				const funcName = Object.keys(type)[0];
+				generatedPassword += randomFunc[funcName]();
+			});
+		}
+		
+		const finalPassword = generatedPassword.slice(0, length);
+		
+		return finalPassword;
+	}
+	// used character codes to locate specific characters
+	// the functions below generate the selected setting to include in the random password
 
-generateEl.addEventListener('click', () => {
-    const length = +lengthEl.value;
-	const hasUpper = uppercaseEl.checked;
-	const hasLower = lowercaseEl.checked;
-	const hasNumber = numbersEl.checked;
-	const hasSymbol = symbolsEl.checked;
-	
-	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
-
-function generatePassword(lower, upper, number, symbol, length) {
-	let generatedPassword = '';
-	const typesCount = lower + upper + number + symbol;
-	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-	
-	// Doesn't have a selected type
-	if(typesCount === 0) {
-		return '';
+	function getRandomUpper() {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+	}
+	function getRandomLower() {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+	}
+	function getRandomNumber() {
+		return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 	}
 	
-	// create a loop
-	for(let i=0; i<length; i+=typesCount) {
-		typesArr.forEach(type => {
-			const funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
-		});
+	function getRandomSymbol() {
+		const symbols = '?\:;"!@#$%^&*(){}[]=<>/,|.'
+		return symbols[Math.floor(Math.random() * symbols.length)];
+		// created a string variable of symbols since there is a wide variety of character codes
 	}
 	
-	const finalPassword = generatedPassword.slice(0, length);
-	
-	return finalPassword;
-}
-// use charcode to get character set for lowercase, random, and number
-
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.?_-\|'
-	return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
 }
